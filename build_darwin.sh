4ce5991957d836fa6ad49466a05af5dcf6506bce
Nathan SALAUN <nathan.salaun@sofia.dev>
2026-03-24 13:21:02 +0100

    Switch to dynamic linking

--- build_darwin.sh
+++ build_darwin.sh
@@ -1,27 +1,27 @@
 set -e
 
 # iOS
-mkdir build_ios
+mkdir -p build_ios
 cd build_ios
-mkdir install
+mkdir -p install
 cmake .. -GXcode -DCMAKE_TOOLCHAIN_FILE="../cmake/ios.toolchain.cmake" -DCMAKE_SYSTEM_NAME="iOS" -DPLATFORM="OS64" -DDEPLOYMENT_TARGET="18.6"
 cmake --build . --config Release
 cmake --install . --prefix "$(realpath install)"
 cd ..
 
 # iOS Simulator
-mkdir build_iphonesimulator
+mkdir -p build_iphonesimulator
 cd build_iphonesimulator
-mkdir install
+mkdir -p install
 cmake .. -GXcode -DCMAKE_TOOLCHAIN_FILE="../cmake/ios.toolchain.cmake" -DCMAKE_SYSTEM_NAME="iOS" -DPLATFORM="SIMULATORARM64" -DDEPLOYMENT_TARGET="18.6"
 cmake --build . --config Release
 cmake --install . --prefix "$(realpath install)"
 cd ..
 
 # macOS
-mkdir build_macos
+mkdir -p build_macos
 cd build_macos
-mkdir install
+mkdir -p install
 cmake .. -GXcode -DDEPLOYMENT_TARGET="15.5"
 cmake --build . --config Release
 cmake --install . --prefix "$(realpath install)"
@@ -29,4 +29,4 @@ cd ..
 
 # Create xcframework
 rm -rf libsentencepiece.xcframework
-xcodebuild -create-xcframework -library build_ios/install/lib/libsentencepiece.a -headers build_ios/install/include -library build_macos/install/lib/libsentencepiece.a -headers build_macos/install/include -library build_iphonesimulator/install/lib/libsentencepiece.a -headers build_iphonesimulator/install/include -output libsentencepiece.xcframework
+xcodebuild -create-xcframework -library build_ios/install/lib/libsentencepiece.0.0.0.dylib -headers build_ios/install/include -library build_macos/install/lib/libsentencepiece.0.0.0.dylib -headers build_macos/install/include -library build_iphonesimulator/install/lib/libsentencepiece.0.0.0.dylib -headers build_iphonesimulator/install/include -output libsentencepiece.xcframework
